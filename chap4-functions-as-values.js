3de15e71a131aee56438878de0a507ebbc6ad386
Hieu Nguyen Duc <hieu@newlogic.com>
2025-12-15 14:05:11 +0700

    add ramda and chap4

---
+++ chap4-functions-as-values.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} word 
+ */
+export const score = (word) => word.replaceAll("a", "").length
+
+/**
+ * @param {string} s 
+ */
+export const numberOfS = (s) => s.length - s.replaceAll("s", "").length
+
+/**
+ * @param {number} i 
+ */
+export const negative = (i) => -i
+
+/**
+ * @param {Array} s
+ */
+export const len = (s) => s.length
