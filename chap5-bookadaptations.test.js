ffd25028d68c9290baa09a0c040d2de151849f4c
Hieu Nguyen Duc <hieu@newlogic.com>
2025-12-15 17:28:55 +0700

    chap5 book adapt

---
+++ chap5-bookadaptations.test.js
@@ -0,0 +1,112 @@
+import { Book, Movie } from "./chap5-bookadaptations"
+import * as R from "ramda"
+
+/** Given the list of all interesting books,
+  * return a feed of movie recommendations.
+  */
+describe('BookAdaptations', () => {
+    test('case 1', () => {
+        const books = [
+            Book({ title: "FP in Scala", authors: ["Chiusano", "Bjarnason"] }),
+            Book({ title: "The Hobbit", authors: ["Tolkien"] }),
+            Book({ title: "Modern Java in Action", authors: ["Urma", "Fusco", "Mycroft"] })
+        ]
+
+        const scalaBooksQty1 = books
+            .map(R.prop('title'))
+            .filter(R.includes("Scala"))
+            .length
+        expect(scalaBooksQty1).toEqual(1)
+
+        const scalaBooksQty2 = books
+            .map(book => book.title)
+            .filter(title => title.includes("Scala"))
+            .length
+        expect(scalaBooksQty2).toEqual(1)
+    })
+
+    test('case 2', () => {
+        const books = [
+            Book({ title: "FP in Scala", authors: ["Chiusano", "Bjarnason"] }),
+            Book({ title: "The Hobbit", authors: ["Tolkien"] })
+        ]
+
+        const bookAdaptations = (author) => (author === "Tolkien")
+            ? [Movie({ title: "An Unexpected Journey" }), Movie({ title: "The Desolation of Smaug" })]
+            : []
+
+        const a1 = books.map(book => book.authors)
+        expect(a1).toEqual([["Chiusano", "Bjarnason"], ["Tolkien"]])
+
+        const a2 = books.map(book => book.authors).flat()
+        expect(a2).toEqual(["Chiusano", "Bjarnason", "Tolkien"])
+
+        const a3 = books.flatMap(R.prop('authors'))
+        expect(a2).toEqual(a3)
+
+        const authors = ["Chiusano", "Bjarnason", "Tolkien"]
+        const movieLists = authors.map(bookAdaptations)
+        expect(movieLists).toEqual([
+            [],
+            [],
+            [Movie({ title: "An Unexpected Journey" }), Movie({ title: "The Desolation of Smaug" })]
+        ])
+        const b1 = movieLists.flat()
+
+        const movies = books
+            .flatMap(R.prop('authors'))
+            .flatMap(bookAdaptations)
+
+        expect(movies).toEqual([Movie({ title: "An Unexpected Journey" }), Movie({ title: "The Desolation of Smaug" })])
+        expect(b1).toEqual(movies)
+
+        { // flatMap and changing the size of the list
+            expect([1, 2, 3].flatMap(i => [i, i + 10]).length).toEqual(6)
+            expect([1, 2, 3].flatMap(i => [i * 2]).length).toEqual(3)
+            expect([1, 2, 3].flatMap(i => i % 2 === 0 ? [i] : []).length).toEqual(1)
+        }
+
+        // see ch05_BookFriendRecommendations
+        // see ch05_SequencedNestedFlatMaps
+
+        const c1 = books
+            .flatMap(book =>
+                book.authors.flatMap(author =>
+                    bookAdaptations(author).map(movie =>
+                        `You may like ${movie.title}, ` +
+                        `because you liked ${author}'s ${book.title}`
+                    )
+                )
+            )
+        expect(c1).toEqual([
+            "You may like An Unexpected Journey, because you liked Tolkien's The Hobbit",
+            "You may like The Desolation of Smaug, because you liked Tolkien's The Hobbit"
+        ])
+
+        const recommendationFeed = (books) =>
+            books.flatMap(book =>
+                book.authors.flatMap(author =>
+                    bookAdaptations(author).map(movie =>
+                        `You may like ${movie.title}, ` +
+                        `because you liked ${author}'s ${book.title}`
+                    )
+                )
+            )
+        expect(recommendationFeed(books)).toEqual([
+            "You may like An Unexpected Journey, because you liked Tolkien's The Hobbit",
+            "You may like The Desolation of Smaug, because you liked Tolkien's The Hobbit"
+        ])
+        // see "Practicing nested flatMaps" in ch05_Points2d3d
+
+        // TODO: rewrite the `for comprehension` in JS
+        //   const c2 = for {
+        //     book   <- books
+        //     author <- book.authors
+        //     movie  <- bookAdaptations(author)
+        //   } yield `You may like ${movie.title}, ` + `because you liked ${author}'s ${book.title}`
+
+        //   expect(c1).toEqual(c2)
+        
+        // see "flatMaps vs. for comprehensions" in ch05_Points2d3d
+    })
+})
